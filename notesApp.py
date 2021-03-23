f140a9c797385b8ecc4a29cef841a4a2243d3879
inamiotko <dupanarolkach121>
2021-03-23 14:02:35 +0100

    Fixed details layout, added bootstrap design to layouts, changed multiple input fields to required.

--- notesApp.py
+++ notesApp.py
@@ -3,24 +3,26 @@ import sqlite3 as sql
 
 app = Flask(__name__)
 
-occurences = []
+occurrences = []
+
+
 @app.route('/')
 def home():
-    # try:
-    #     conn = sql.connect('database.db')
-    #     print("połączono z bazą danych")
-    #     print("Opened database successfully")
-    #
-    #     conn.execute('CREATE TABLE notes (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL, added TEXT NOT NULL)')
-    #     conn.execute('CREATE TABLE items (id INTEGER PRIMARY KEY AUTOINCREMENT, note_id INTEGER NOT NULL,title TEXT '
-    #                  'NOT NULL, added TEXT NOT NULL , modified TEXT NOT NULL, content TEXT NOT NULL, deleted INT NOT '
-    #                  'NULL, edited INT NOT NULL, FOREIGN KEY (note_id) REFERENCES notes(id),'
-    #                  'UNIQUE(note_id,title,edited,deleted) ON CONFLICT IGNORE)')
-    #     print("Tables created successfully")
-    # except sql.Error as error:
-    #         print("Failed to create table", error)
-    #
-    # conn.close()
+    try:
+        conn = sql.connect('database.db')
+        print("Opened database successfully")
+
+        conn.execute('CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL, '
+                     'added TEXT NOT NULL)')
+        conn.execute('CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY AUTOINCREMENT, note_id INTEGER NOT '
+                     'NULL,title TEXT NOT NULL, added TEXT NOT NULL , modified TEXT NOT NULL, content TEXT NOT NULL, '
+                     'deleted INT NOT NULL, edited INT NOT NULL, FOREIGN KEY (note_id) REFERENCES notes(id),'
+                     'UNIQUE(note_id,title,edited,deleted) ON CONFLICT IGNORE)')
+        print("Tables opened successfully")
+    except sql.Error as error:
+        print("Failed to create table ", error)
+
+    conn.close()
     return render_template('index.html')
 
 
@@ -32,19 +34,17 @@ def addtodb():
             title = request.form['title']
             content = request.form['content']
             created = request.form['added']
-
-            cur = conn.cursor()
             conn.execute("INSERT INTO notes (title, added) VALUES(?, ?)", (title, created))
 
             conn.execute(
                 "INSERT INTO items (deleted, edited, title,content, added, modified, note_id) SELECT ?,?,?,?,?,?, "
                 "id FROM notes WHERE id=id AND NOT EXISTS (SELECT 1 FROM items WHERE (items.title = notes.title AND "
-                "items.note_id=notes.id))",(0, 0, title, content, created, created))
+                "items.note_id=notes.id))", (0, 0, title, content, created, created))
             conn.commit()
-            print("note added")
+            print("Note added")
 
         except sql.Error as error:
-            print("Failed to update sqlite table", error)
+            print("Failed to add record ", error)
 
         return render_template("added.html")
         conn.close()
@@ -52,11 +52,14 @@ def addtodb():
 
 @app.route('/results')
 def results():
-    conn = sql.connect('database.db')
-    cur = conn.cursor()
-    conn.row_factory = sql.Row
-    cur.execute("SELECT * FROM notes")
-    data = cur.fetchall()
+    try:
+        conn = sql.connect('database.db')
+        cur = conn.cursor()
+        conn.row_factory = sql.Row
+        cur.execute("SELECT * FROM notes")
+        data = cur.fetchall()
+    except sql.Error as error:
+        print("Failed to make query ", error)
     return render_template('results.html', data=data)
 
 
@@ -65,13 +68,17 @@ def update():
     if request.method == 'POST':
         try:
             conn = sql.connect('database.db')
+            cur = conn.cursor()
             id_up = request.form['id']
             title_up = request.form['title']
-            content_up = request.form['content']
-            print(id_up)
-        except:
-            print("ex")
-    return render_template('update.html', id=id_up, title=title_up, content=content_up)
+            cur.execute("SELECT content FROM items WHERE (note_id=? AND title=?)", (id_up, title_up))
+            content_up = cur.fetchall()
+            content = content_up.pop()
+            content = content[0]
+
+        except sql.Error as error:
+            print("Failed to update table ", error)
+    return render_template('update.html', id=id_up, title=title_up, content=content)
 
 
 @app.route('/edit', methods=['POST', 'GET'])
@@ -85,8 +92,8 @@ def edit():
             modified = request.form['modifiedN']
             print(title, content, id_up, modified)
             cur = conn.cursor()
-            occurences.append(id_up)
-            ver = occurences.count(id_up)
+            occurrences.append(id_up)
+            ver = occurrences.count(id_up)
             conn.execute("INSERT INTO items (note_id, edited, deleted,title,content, added, modified) SELECT note_id, "
                          "? ,deleted,?,?, added, ? FROM items WHERE note_id=?", (ver, title, content, modified, id_up))
             conn.execute("UPDATE notes SET title=? WHERE id=? ", (title, id_up))
@@ -94,31 +101,37 @@ def edit():
             print("note changed")
             cur.close()
         except sql.Error as error:
-            print("Failed to update sqlite table", error)
+            print("Failed to edit table ", error)
     return render_template('added.html')
 
 
 @app.route('/delete', methods=['POST', 'GET'])
 def delete():
-    conn = sql.connect('database.db')
-    cur = conn.cursor()
-    id_del = request.form['delete']
-    cur.execute("DELETE FROM notes WHERE id=?", (id_del,))
-    cur.execute("UPDATE items SET deleted =? WHERE note_id=? ", (1, id_del))
-    conn.commit()
-    data_aft = cur.fetchall()
-    return render_template('results.html', data=data_aft)
+    try:
+        conn = sql.connect('database.db')
+        cur = conn.cursor()
+        id_del = request.form['delete']
+        cur.execute("DELETE FROM notes WHERE id=?", (id_del,))
+        cur.execute("UPDATE items SET deleted =? WHERE note_id=? ", (1, id_del))
+        conn.commit()
+        data_aft = cur.fetchall()
+    except sql.Error as error:
+        print("Failed to delete record ", error)
+    return render_template('delete.html', data=data_aft)
 
 
 @app.route('/details', methods=['POST', 'GET'])
 def details():
-    conn = sql.connect('database.db')
-    cur = conn.cursor()
-    id_show = request.form['details']
-    cur.execute("SELECT * FROM items WHERE note_id=?", id_show)
-    conn.commit()
-    data_aft = cur.fetchall()
-    return render_template('results.html', data=data_aft)
+    try:
+        conn = sql.connect('database.db')
+        cur = conn.cursor()
+        id_show = request.form['details']
+        cur.execute("SELECT * FROM items WHERE note_id=? ORDER BY edited ASC", id_show)
+        conn.commit()
+        data_aft = cur.fetchall()
+    except sql.Error as error:
+        print("Failed to query details ", error)
+    return render_template('details.html', data=data_aft)
 
 
 if __name__ == '__main__':
